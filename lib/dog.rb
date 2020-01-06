1f993815e90595fb90f89a4ad247f0e3e1471645
Alex Martinez <alexandr3martin3z@gmail.com>
2020-01-06 03:05:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,6 +52,16 @@ class Dog
   end
 
   def self.find_by_id
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def find_or_create_by(name:, breed:)
